b7ff3328ed87f51cc046c50058a642da66bead5d
Meric Ozkayagan <meric.ozkayagan@navlungo.com>
2023-02-24 23:43:19 +0300

    update descriptions

--- cmd/createuser.go
+++ cmd/createuser.go
@@ -16,13 +16,8 @@ import (
 // createuserCmd represents the createuser command
 var createuserCmd = &cobra.Command{
 	Use:   "createuser",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create a new user",
+	Long: `Create a new user with a policy attached to it`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("createuser called")
 	},
--- cmd/init.go
+++ cmd/init.go
@@ -14,13 +14,8 @@ import (
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Creates the ec2 instance",
+	Long: `Creates the ec2 instance and associates the elastic ip`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
 	},
